0bb2c2f26c34a196a002ed7a4a8bcc41215a6629
Benjamin Howe <ben@bh96.uk>
2017-03-26 08:55:47 +0100

    Added ShellInABox (proxied via nginx), web shell now works, fixes #3

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -3,3 +3,11 @@
 install -v -d                                           ${ROOTFS_DIR}/etc/wpa_supplicant
 install -v -m 600 files/wpa_supplicant.conf             ${ROOTFS_DIR}/etc/wpa_supplicant/
 
+install -v -m 644 files/default                         ${ROOTFS_DIR}/etc/nginx/sites-available/
+
+install -v -m 644 files/shellinabox                     ${ROOTFS_DIR}/etc/default/
+mv ${ROOTFS_DIR}/etc/shellinabox/options-available/00+Black\ on\ White.css ${ROOTFS_DIR}/etc/shellinabox/options-available/00_Black\ on\ White.css
+mv ${ROOTFS_DIR}/etc/shellinabox/options-available/00_White\ On\ Black.css ${ROOTFS_DIR}/etc/shellinabox/options-available/00+White\ On\ Black.css
+rm ${ROOTFS_DIR}/etc/shellinabox/options-enabled/00*
+ln -s /etc/shellinabox/options-available/00_Black\ on\ White.css ${ROOTFS_DIR}/etc/shellinabox/options-enabled/00_Black\ on\ White.css
+ln -s /etc/shellinabox/options-available/00+White\ On\ Black.css ${ROOTFS_DIR}/etc/shellinabox/options-enabled/00+White\ On\ Black.css
